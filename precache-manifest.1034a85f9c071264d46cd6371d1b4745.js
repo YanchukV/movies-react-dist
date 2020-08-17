181357f1068ee55f28643c84909c143fbcffa7ae
Vyacheslav <jantschuk@gmail.com>
2020-08-17 17:48:55 +0300

    first

---
+++ precache-manifest.1034a85f9c071264d46cd6371d1b4745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0437447b26d1e7602b4d7873d11b8d",
+    "url": "/movies-react-dist/index.html"
+  },
+  {
+    "revision": "afc70cef7d9405a5f217",
+    "url": "/movies-react-dist/static/css/main.c1a25379.chunk.css"
+  },
+  {
+    "revision": "1a28ed076bc40baff618",
+    "url": "/movies-react-dist/static/js/2.93868376.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/movies-react-dist/static/js/2.93868376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc70cef7d9405a5f217",
+    "url": "/movies-react-dist/static/js/main.43cda910.chunk.js"
+  },
+  {
+    "revision": "da531c8b647584dfa6b2",
+    "url": "/movies-react-dist/static/js/runtime-main.cc518810.js"
+  }
+]);
